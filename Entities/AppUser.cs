19da711ca8b4b18eb9112989d820e1d3e77f5aba
Juantrevi <juantrevi70@gmail.com>
2023-10-19 18:47:13 +0100

    -- BaseController --

--- Entities/AppUser.cs
+++ Entities/AppUser.cs
@@ -6,5 +6,9 @@
 
         public string UserName { get; set; }
 
+        public byte[] Password { get; set; }
+
+        public byte[] PasswordSalt { get; set; }
+
     }
 }
--- Program.cs
+++ Program.cs
@@ -5,8 +5,6 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-
-
 builder.Services.AddControllers();
 
 builder.Services.AddDbContext<DataContext>(opt =>
