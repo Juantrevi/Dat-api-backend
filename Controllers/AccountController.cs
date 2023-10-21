f6a82a17bb70cbf28d5a66d268485f9c426ff855
Juantrevi <juantrevi70@gmail.com>
2023-10-21 11:58:35 +0100

    -- Login --

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -43,6 +43,28 @@ namespace Dat_api.Controllers
 
         }
 
+        [HttpPost("login")] //POST //api/accounts/login 
+        public async Task<ActionResult<AppUser>> Login(LoginDto loginDto)
+        {
+            var user = await _context.Users.SingleOrDefaultAsync(x => x.UserName == loginDto.Username);
+
+            if(user == null) return Unauthorized("Invalid username");
+
+            //using is used to dispose of the hmac object after it is used (Garbage Collection)
+            using var hmac = new HMACSHA512(user.PasswordSalt);
+
+            var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+
+            for(int i = 0; i < computedHash.Length; i++)
+            {
+                if (computedHash[i] != user.Password[i]) return Unauthorized("Invalid password");
+            }
+
+            return user;
+
+        }   
+
+
         private async Task<bool> UserExists(string username)
         {
             return await _context.Users.AnyAsync(x => x.UserName.ToLower() == username.ToLower());
---
+++ DTOs/LoginDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Dat_api.DTOs
+{
+    public class LoginDto
+    {
+
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+    }
+}
