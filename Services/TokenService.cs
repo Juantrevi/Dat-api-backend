54a1ebcf31ff7365728bc337a7bc7921161db045
Juantrevi <juantrevi70@gmail.com>
2023-10-21 13:24:49 +0100

    -- JWT stage 2 --

--- Services/TokenService.cs
+++ Services/TokenService.cs
@@ -1,13 +1,26 @@
-﻿using Dat_api.Entities;
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Dat_api.Entities;
 using Dat_api.Interfaces;
+using Microsoft.IdentityModel.Tokens;
 
 namespace Dat_api.Services
 {
     public class TokenService : ITokenService
     {
+        private readonly SymmetricSecurityKey _key;
+        public TokenService(IConfiguration config)
+        {
+            _key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["TokenKey"]));
+        }
+
         public string CreateToken(AppUser user)
         {
-            throw new NotImplementedException();
+            var claims = new List<Claim> //claims are the things we want to store inside the token
+            {
+                new Claim(JwtRegisteredClaimNames.NameId, user.UserName)
+            };
         }
     }
 }
