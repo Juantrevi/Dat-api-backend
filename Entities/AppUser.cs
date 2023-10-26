1e426260ee1227c5aaa725f6f6d9c01d9517c844
Juantrevi <juantrevi70@gmail.com>
2023-10-26 18:53:51 +0100

    -- Adding properties to users and photos --

--- Entities/AppUser.cs
+++ Entities/AppUser.cs
@@ -1,4 +1,6 @@
 ﻿
+using Dat_api.Extensions;
+
 namespace Dat_api.Entities
 {
     public class AppUser
@@ -10,6 +12,38 @@ namespace Dat_api.Entities
         public byte[] Password { get; set; }
 
         public byte[] PasswordSalt { get; set; }
+
+        public DateOnly DateOfBirth { get; set; }
+
+        public string KnownAs { get; set; }
+
+        public DateTime Created { get; set; } = DateTime.UtcNow;
+
+        public DateTime LastActive { get; set; } = DateTime.UtcNow;
+
+        public string Gender { get; set; }
+
+        public string Introduction { get; set; }
+
+        public string LookingFor { get; set; }
+
+        public string Interests { get; set; }
+
+        public string City { get; set; }
+
+        public string Country { get; set; }
+
+
+        public List<Photo> Photos { get; set; } = new();
+
+        public int GetAge()
+        {
+            return DateOfBirth.calculateAge();
+        }
+
+
+
+
 
     }
 }
---
+++ Extensions/DateTimeExtensions.cs
@@ -0,0 +1,19 @@
+﻿namespace Dat_api.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        public static int calculateAge(this DateOnly dateOfBirth)
+        {
+            var today = DateOnly.FromDateTime(DateTime.UtcNow);
+
+            var age = today.Year - dateOfBirth.Year;
+
+            if (dateOfBirth.AddYears(age) > today)
+            {
+                age--;
+            }
+
+            return age;
+        }
+    }
+}
