d25833524363b55776eaaa6ea783d2efb24b3778
Juantrevi <juantrevi70@gmail.com>
2023-11-04 19:39:34 +0000

    -- Account --

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -51,8 +51,9 @@ namespace Dat_api.Controllers
             {
                 Username = user.UserName,
                 Token = _tokenService.CreateToken(user),
-                KnownAs = user.KnownAs
+                KnownAs = user.KnownAs,
                 //,PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain)?.Url
+                Gender = user.Gender,
             };
 
         }
@@ -81,7 +82,8 @@ namespace Dat_api.Controllers
                 Username = user.UserName,
                 Token = _tokenService.CreateToken(user),
                 PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain)?.Url,
-                KnownAs = user.KnownAs
+                KnownAs = user.KnownAs,
+                Gender = user.Gender,
             };
 
         }   
--- DTOs/UserDto.cs
+++ DTOs/UserDto.cs
@@ -9,5 +9,7 @@
 
         public string KnownAs { get; set; }
 
+        public string Gender { get; set; }
+
     }
 }
