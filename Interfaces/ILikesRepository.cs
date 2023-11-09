7593d1ee81a851b6430bf8cf4825f69a280dca5a
Juantrevi <juantrevi70@gmail.com>
2023-11-09 11:55:31 +0000

    ----

---
+++ Interfaces/ILikesRepository.cs
@@ -0,0 +1,13 @@
+﻿using Dat_api.Entities;
+
+namespace Dat_api.Interfaces
+{
+	public interface ILikesRepository
+	{
+		Task<UserLike> GetUserLike(int sourceUserId, int likedUserId);
+
+		Task<AppUser> GetUserWithLikes(int userId);
+
+		Task<IEnumerable<LikeDto>> GetUserLikes(string predicate, int userId);
+	}
+}
