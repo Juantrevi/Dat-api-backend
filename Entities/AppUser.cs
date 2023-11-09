1f29330a9dafee47d31740ae5ba56901ee49b9c5
Juantrevi <juantrevi70@gmail.com>
2023-11-09 11:32:05 +0000

    -- Like feature --

--- Entities/AppUser.cs
+++ Entities/AppUser.cs
@@ -33,13 +33,11 @@ namespace Dat_api.Entities
 
         public string Country { get; set; }
 
-
         public List<Photo> Photos { get; set; } = new();
 
-/*        public int GetAge()
-        {
-            return DateOfBirth.calculateAge();
-        }*/
+        public List<AppUser> LikedByUsers { get; set; }
+
+        public List<AppUser> LikedUsers { get; set; }
 
 
 
