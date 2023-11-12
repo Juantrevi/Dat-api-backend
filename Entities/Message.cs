294049c974e5328caef3ba677bc17e3c59773d30
Juantrevi <juantrevi70@gmail.com>
2023-11-12 13:16:44 +0000

    -- Entity message --

---
+++ Entities/Message.cs
@@ -0,0 +1,19 @@
+﻿namespace Dat_api.Entities
+{
+	public class Message
+	{
+		public int Id { get; set; }
+		public int senderId { get; set; }
+		public string senderUsername { get; set; }
+		public AppUser sender { get; set; }
+		public int RecipientId { get; set; }
+		public string RecipientUsername { get; set; }
+		public AppUser Recipient { get; set; }
+		public string Content { get; set; }
+		public DateTime? DateRead { get; set; }
+		public DateTime MessageSent { get; set; } = DateTime.UtcNow;
+		public bool SenderDeleted { get; set; }
+		public bool RecipientDeleted { get; set; }
+
+	}
+}
