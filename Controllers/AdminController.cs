5f2015d98833584fd67ecaf2ba5d0ac8a9749889
Juantrevi <juantrevi70@gmail.com>
2023-11-19 13:17:38 +0000

    -- Capacity of adding Roles --

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 namespace Dat_api.Controllers
 {
@@ -32,6 +33,36 @@ namespace Dat_api.Controllers
 				.ToListAsync();
 
 			return Ok(user);
+
+		}
+
+
+		[Authorize(Policy = "RequireAdminRole")]
+		[HttpPost("edit-roles/{username}")]
+		public async Task<ActionResult> EditRoles(string username, [FromQuery]string roles)
+		{
+
+			if (string.IsNullOrEmpty(roles)) return BadRequest("You must select at least one role");
+
+			//We are going to pass the roles as separeted by comma, so now we need to split them and put them in an array
+			var selectedRoles = roles.Split(",").ToArray();
+
+			var user = await _userManager.FindByNameAsync(username);
+
+			if (user == null) return NotFound("Could not find user");
+
+			var userRoles = await _userManager.GetRolesAsync(user);
+
+			var result = await _userManager.AddToRolesAsync(user, selectedRoles.Except(userRoles));
+
+			if (!result.Succeeded) return BadRequest("Failed to add to roles");
+
+			result = await _userManager.RemoveFromRolesAsync(user, userRoles.Except(selectedRoles));
+
+			if (!result.Succeeded) return BadRequest("Failed to remove from roles");
+
+			return Ok(await _userManager.GetRolesAsync(user));
+
 
 		}
 
