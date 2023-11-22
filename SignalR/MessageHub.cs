4bc5c784ac95a42202c1046c709e655f26fa6372
Juantrevi <juantrevi70@gmail.com>
2023-11-22 21:16:00 +0000

    -- Working hub messages read --

--- SignalR/MessageHub.cs
+++ SignalR/MessageHub.cs
@@ -31,6 +31,8 @@ namespace Dat_api.SignalR
 			var groupName = GetGroupName(Context.User.GetUsername(), otherUser);
 			await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
 
+			await AddToGroup(groupName);
+
 			var messages = await _messageRepository.GetMessageThread(Context.User.GetUsername(), otherUser);
 
 			await Clients.Group(groupName).SendAsync("ReceiveMessageThread", messages);
@@ -40,6 +42,8 @@ namespace Dat_api.SignalR
 
 		public override async Task OnDisconnectedAsync(Exception exception)
 		{
+			await RemoveFromMessageGroup();
+
 			await base.OnDisconnectedAsync(exception);
 		}
 
@@ -81,14 +85,51 @@ namespace Dat_api.SignalR
 				Content = createMessageDto.Content
 			};
 
+			var groupName = GetGroupName(sender.UserName, recipient.UserName);
+
+			var group = await _messageRepository.GetMessageGroup(groupName);
+
+			if (group.Connections.Any(x => x.Username == recipient.UserName))
+			{
+				message.DateRead = DateTime.UtcNow;
+			}
+
 			_messageRepository.AddMessage(message);
 
 			if (await _messageRepository.SaveAllAsync())
 			{
-				var group = GetGroupName(sender.UserName, recipient.UserName);
-				await Clients.Group(group).SendAsync("NewMessage", _mapper.Map<MessageDto>(message));
+
+				await Clients.Group(groupName).SendAsync("NewMessage", _mapper.Map<MessageDto>(message));
 
 			}
+
+		}
+
+
+		private async Task<bool> AddToGroup(string groupName)
+		{
+			var group = await _messageRepository.GetMessageGroup(groupName);
+			var connection = new Connection(Context.ConnectionId, Context.User.GetUsername());
+
+			if (group == null)
+			{
+				group = new Group(groupName);
+				_messageRepository.AddGroup(group);
+			}
+
+			group.Connections.Add(connection);
+
+			return await _messageRepository.SaveAllAsync();
+		}
+
+
+		private async Task RemoveFromMessageGroup()
+		{
+			var connection = await _messageRepository.GetConnection(Context.ConnectionId);
+
+			_messageRepository.RemoveConnection(connection);
+
+			await _messageRepository.SaveAllAsync();
 
 		}
     }
