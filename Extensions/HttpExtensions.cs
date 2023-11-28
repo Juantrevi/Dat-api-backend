1753c02beb3648f3ce4cf7bdbc4fd6f5dd49ba24
Juantrevi <juantrevi70@gmail.com>
2023-11-28 22:01:36 +0000

    -- Pagination correctly --

--- Extensions/HttpExtensions.cs
+++ Extensions/HttpExtensions.cs
@@ -8,11 +8,13 @@ namespace Dat_api.Extensions
 
         public static void AddPaginationHeader(this HttpResponse response, PaginationHeader header)
         {
+
             var jsonOptions = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
 
             response.Headers.Add("Pagination", JsonSerializer.Serialize(header, jsonOptions));
 
             response.Headers.Add("Access-Control-Expose-Headers", "Pagination");
+
         }
 
 
--- Extensions/IdentityServiceExtensions.cs
+++ Extensions/IdentityServiceExtensions.cs
@@ -53,8 +53,10 @@ namespace Dat_api.Extentions
 
             services.AddAuthorization(opt =>
             {
+
 				opt.AddPolicy("RequireAdminRole", policy => policy.RequireRole("Admin"));
 				opt.AddPolicy("ModeratePhotoRole", policy => policy.RequireRole("Admin", "Moderator"));
+
 			});
 
 
